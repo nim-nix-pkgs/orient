22e5b8592eee13fe4da105302981ec026eba01c2
hugosenari <hugosenari@gmail.com>
2022-03-27 18:25:25 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-orient-master.ref   = "refs/heads/master";
   inputs.src-orient-master.owner = "philip-wernersbach";
   inputs.src-orient-master.repo  = "nim-orient";
+  inputs.src-orient-master.dir   = "";
   inputs.src-orient-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
